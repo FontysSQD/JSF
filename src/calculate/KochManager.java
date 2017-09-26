99394de6478d900303751171050bca101bc531d8
Quint Aartsen <quintaartsen@MacBook-Pro-van-Quint.local>
2017-09-26 11:52:23 +0200

    Add threading

--- src/calculate/KochManager.java
+++ src/calculate/KochManager.java
@@ -1,37 +1,80 @@
 package calculate;
 
+import javafx.application.Platform;
 import jsf31kochfractalfx.JSF31KochFractalFX;
 import timeutil.TimeStamp;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
-public class KochManager implements Observer {
-    private KochFractal kochFractal = new KochFractal();
+public class KochManager {
     private JSF31KochFractalFX application;
     private ArrayList<Edge> edges = new ArrayList<>();
-
+    private int count = 0;
     public KochManager(JSF31KochFractalFX application) {
         this.application = application;
-        kochFractal.addObserver(this::update);
     }
 
     public void changeLevel(int nxt) {
-        kochFractal.setLevel(nxt);
-
+        edges.clear();
         TimeStamp ts = new TimeStamp();
         ts.setBegin();
-        kochFractal.generateLeftEdge();
-        kochFractal.generateBottomEdge();
-        kochFractal.generateRightEdge();
+        Thread t1 = new Thread(new ThreadManager(nxt) {
+            @Override
+            public void run() {
+                synchronized (edges) {
+                    generateLeftEdge();
+                    count++;
+                    if(count >= 3) {
+                        application.requestDrawEdges();
+                        count = 0;
+                    }
+                    edges.addAll(this.edge);
+                }
+            }
+        });
+        t1.start();
+
+        Thread t2 = new Thread(new ThreadManager(nxt) {
+            @Override
+            public void run() {
+                synchronized (edges) {
+                    generateBottomEdge();
+                    count++;
+                    if(count >= 3) {
+                        application.requestDrawEdges();
+                        count = 0;
+                    }
+                    edges.addAll(this.edge);
+                }
+            }
+        });
+        t2.start();
+
+        Thread t3 = new Thread(new ThreadManager(nxt) {
+            @Override
+            public void run() {
+                synchronized (edges) {
+                    generateRightEdge();
+                    count++;
+                    if(count >= 3) {
+                        application.requestDrawEdges();
+                        count = 0;
+                    }
+                    edges.addAll(this.edge);
+                }
+            }
+        });
+        t3.start();
         ts.setEnd("Einde generate");
 
-        application.setTextCalc(ts.toString());
-        drawEdges();
+       // application.setTextCalc(ts.toString());
     }
 
     public void drawEdges() {
+
         application.clearKochPanel();
 
         TimeStamp ts = new TimeStamp();
@@ -42,11 +85,6 @@ public class KochManager implements Observer {
         ts.setEnd("Einde tekenen");
 
         application.setTextDraw(ts.toString());
-        application.setTextNrEdges(Integer.toString(kochFractal.getNrOfEdges()));
-    }
-
-    @Override
-    public void update(Observable o, Object arg) {
-        edges.add((Edge)arg);
+        // application.setTextNrEdges(Integer.toString(kochFractal.getNrOfEdges()));
     }
 }
