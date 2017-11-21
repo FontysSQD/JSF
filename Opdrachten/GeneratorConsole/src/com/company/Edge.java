d760d6cd1483bd6a9a9f5fa5a14f365f3448b342
Sven <s_devries@live.nl>
2017-11-21 11:07:53 +0100

    Applicatie1 week 12

---
+++ Opdrachten/GeneratorConsole/src/com/company/Edge.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import javafx.scene.paint.Color;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+public class Edge implements Externalizable{
+    public double X1, Y1, X2, Y2;
+    public Color color;
+    
+    public Edge(double X1, double Y1, double X2, double Y2, Color color) {
+        this.X1 = X1;
+        this.Y1 = Y1;
+        this.X2 = X2;
+        this.Y2 = Y2;
+        this.color = color;
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.write((int)X1);
+        out.write((int)Y1);
+        out.write((int)X2);
+        out.write((int)Y2);
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+
+    }
+}
---
+++ Opdrachten/GeneratorConsole/src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Scanner;
+
+public class Main implements Observer{
+    private static final String fileName = "C:\\test\\generatorConsole.txt";
+    ArrayList<Edge> edges = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        while(true){
+            main.edges.clear();
+            KochFractal kochFractal = new KochFractal();
+            kochFractal.addObserver(main);
+            System.out.print("Welk level moeten de edges gegenereerd worden? \n");
+            Scanner input = new Scanner(System.in);
+            String lvlString = input.nextLine();
+            int level = Integer.valueOf(lvlString);
+            kochFractal.setLevel(level);
+            kochFractal.generateBottomEdge();
+            kochFractal.generateLeftEdge();
+            kochFractal.generateRightEdge();
+            System.out.print("Totaal aantal edges: " + kochFractal.getNrOfEdges() + "\n");
+            main.writeToFile();
+        }
+    }
+
+    public void writeToFile(){
+        try (ObjectOutputStream oos = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(fileName)))) {
+            for(Edge e : edges){
+                oos.writeObject(e);
+            }
+            System.out.println("Wegschrijven geslaagd");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        edges.add((Edge)arg);
+    }
+}
