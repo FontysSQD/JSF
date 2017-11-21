f16cc450433bf9253a38b3f13f6617d3afc96d33
Dane Naebers <danenaebers@gmail.com>
2017-11-21 14:11:24 +0100

    Read Methods in FileManager

--- Opdrachten/GuiApplicatie/src/calculator/FileManager.java
+++ Opdrachten/GuiApplicatie/src/calculator/FileManager.java
@@ -1,5 +1,8 @@
 package calculator;
 
+import javafx.scene.paint.Color;
+
+import java.awt.*;
 import java.io.*;
 import java.util.ArrayList;
 
@@ -8,12 +11,13 @@ import java.util.ArrayList;
  */
 public class FileManager {
 
-    private ArrayList<Edge> edges ;
-    public ArrayList<Edge> getFileNoBuffer(int nxt) {
+    private ArrayList<Edge> edges;
+
+    public ArrayList<Edge> getObjectNoBuffer(int nxt) {
         edges = new ArrayList<>();
         String fileName = "/home/dane/Desktop/lv" + String.valueOf(nxt) + ".edgyboi";
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName))) {
-            while(true) {
+            while (true) {
                 Edge e = (Edge) ois.readObject();
                 if (e != null) {
                     edges.add(e);
@@ -30,11 +34,11 @@ public class FileManager {
         return edges;
     }
 
-    public ArrayList<Edge> getFileWithBuffer(int nxt) {
+    public ArrayList<Edge> getObjectWithBuffer(int nxt) {
         edges = new ArrayList<>();
-        String fileName = "/home/dane/Desktop/lv" + String.valueOf(nxt) + ".edgyboi";
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName))) {
-            while(true) {
+        String fileName = "/home/dane/Desktop/blv" + String.valueOf(nxt) + ".edgyboi";
+        try (ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream(fileName)))) {
+            while (true) {
                 Edge e = (Edge) ois.readObject();
                 if (e != null) {
                     edges.add(e);
@@ -50,5 +54,39 @@ public class FileManager {
         }
 
         return edges;
+    }
+
+    public ArrayList<Edge> getStringWithBuffer(int nxt) {
+        edges = new ArrayList<>();
+        String fileName = "/home/dane/Desktop/blv" + String.valueOf(nxt) + ".edgystringyboi";
+        try (InputStreamReader isr = new InputStreamReader(new BufferedInputStream(new FileInputStream(fileName)))) {
+            parseStringFiles(String.valueOf(isr.read()));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return edges;
+    }
+
+    public ArrayList<Edge> getStringNoBuffer(int nxt) {
+        edges = new ArrayList<>();
+        String fileName = "/home/dane/Desktop/lv" + String.valueOf(nxt) + ".edgystringyboi";
+        try (InputStreamReader isr = new InputStreamReader(new FileInputStream(fileName))) {
+           parseStringFiles(String.valueOf(isr.read()));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return edges;
+    }
+
+    private void parseStringFiles(String zooj) {
+        String[] objects =  zooj.split(";");
+        for(String object : objects) {
+            String[] fields = object.split(",");
+            edges.add(new Edge(Double.parseDouble(fields[0]), Double.parseDouble(fields[0]), Double.parseDouble(fields[0]), Double.parseDouble(fields[0]), Color.CHOCOLATE));
+        }
     }
 }
