e5e4d55ca4c5a4d86eed5f0c1d134a3407eca9b8
Sven <s_devries@live.nl>
2017-11-28 11:02:25 +0100

    week 13

--- Opdrachten/GuiApplicatie/src/calculator/KochManager.java
+++ Opdrachten/GuiApplicatie/src/calculator/KochManager.java
@@ -4,28 +4,60 @@ import javafx.scene.paint.Color;
 import jsf31kochfractalfx.JSF31KochFractalFX;
 import timeutil.TimeStamp;
 
+import java.io.*;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 
 public class KochManager {
     private JSF31KochFractalFX application;
     private FileManager fileManager;
     private ArrayList<Edge> edges = new ArrayList<>();
+    TimeStamp ts = new TimeStamp();
 
     public KochManager(JSF31KochFractalFX application) {
         this.application = application;
-        fileManager = new FileManager();
+//        fileManager = new FileManager();
     }
 
     public void changeLevel(int nxt) {
-        edges = fileManager.getStringNoBuffer(nxt);
+//        edges = fileManager.getStringNoBuffer(nxt);
 //        edges = fileManager.getObjectNoBuffer(nxt);
 //        edges = fileManager.getStringWithBuffer(nxt);
 //        edges = fileManager.getObjectWithBuffer(nxt);
+
+        edges.clear();
+        try {
+            ts.init();
+            ts.setBegin("Begin read mappedByteBuffer without buffer");
+            RandomAccessFile ras = new RandomAccessFile(String.valueOf(nxt) + ".dat", "rw");
+            FileChannel fc = ras.getChannel();
+            MappedByteBuffer buffer = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+            byte[] bytes = new byte[buffer.limit()];
+
+            for (int i = 0; i < buffer.limit(); i++) {
+                bytes[i] = buffer.get(i);
+            }
+
+            ByteArrayInputStream in = new ByteArrayInputStream(bytes);
+            ObjectInputStream is = new ObjectInputStream(in);
+            edges.addAll((ArrayList<Edge>) is.readObject());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            ts.setEnd("End");
+            System.out.println(ts.toString());
+        }
+
         drawEdges();
     }
 
     public void drawEdges() {
-        if(!edges.isEmpty()){
+        if (!edges.isEmpty()) {
             application.clearKochPanel();
 
             TimeStamp ts = new TimeStamp();
--- Opdrachten/GuiApplicatie/src/calculator/Main.java
+++ Opdrachten/GuiApplicatie/src/calculator/Main.java
@@ -3,19 +3,21 @@ package calculator;
 import timeutil.TimeStamp;
 
 import java.io.*;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.Scanner;
 
-public class Main implements Observer{
+public class Main implements Observer {
     private static int level;
     ArrayList<Edge> edges = new ArrayList<>();
     TimeStamp ts = new TimeStamp();
 
     public static void main(String[] args) {
         Main main = new Main();
-        while(true){
+        while (true) {
             main.edges.clear();
             KochFractal kochFractal = new KochFractal();
             kochFractal.addObserver(main);
@@ -27,80 +29,99 @@ public class Main implements Observer{
             kochFractal.generateLeftEdge();
             kochFractal.generateRightEdge();
             System.out.print("Totaal aantal edges: " + kochFractal.getNrOfEdges() + "\n");
-            main.writeToByteWithoutBuffer();
-            main.writeToByteWithBuffer();
-            main.writeToTextWithoutBuffer();
-            main.writeToTextWithBuffer();
+//            main.writeToByteWithoutBuffer();
+//            main.writeToByteWithBuffer();
+//            main.writeToTextWithoutBuffer();
+//            main.writeToTextWithBuffer();
+            try {
+                main.ts.init();
+                main.ts.setBegin("Begin write byte without buffer");
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                ObjectOutputStream os = new ObjectOutputStream(out);
+                os.writeObject(main.edges);
+
+                RandomAccessFile ras = new RandomAccessFile(String.valueOf(level) + ".dat", "rw");
+                FileChannel fc = ras.getChannel();
+                MappedByteBuffer buffer = fc.map(FileChannel.MapMode.READ_WRITE, 0, out.toByteArray().length);
+
+                for (Byte b : out.toByteArray()) {
+                    buffer.put(b);
+                }
+                System.out.println("Finished writing");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                main.ts.setEnd("End");
+                System.out.println(main.ts.toString());
+            }
         }
     }
 
-    public void writeToByteWithoutBuffer(){
+    public void writeToByteWithoutBuffer() {
         ts.init();
         ts.setBegin("Begin write byte without buffer");
         String fileName = "C:\\test\\lv" + String.valueOf(level) + ".edgyboi";
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName))) {
-            for(Edge e : edges){
+            for (Edge e : edges) {
                 oos.writeObject(e);
             }
             System.out.println("Wegschrijven byte zonder buffer geslaagd");
         } catch (IOException e) {
             e.printStackTrace();
-        }
-        finally {
+        } finally {
             ts.setEnd("End");
             System.out.println(ts.toString());
         }
     }
 
-    public void writeToByteWithBuffer(){
+    public void writeToByteWithBuffer() {
         ts.init();
         ts.setBegin("Begin write byte with buffer");
         String fileName = "C:\\test\\blv" + String.valueOf(level) + ".edgyboi";
         try (ObjectOutputStream oos = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(fileName)))) {
-            for(Edge e : edges){
+            for (Edge e : edges) {
                 oos.writeObject(e);
             }
             System.out.println("Wegschrijven byte met buffer geslaagd");
         } catch (IOException e) {
             e.printStackTrace();
-        }
-        finally {
+        } finally {
             ts.setEnd("End");
             System.out.println(ts.toString());
         }
     }
 
-    public void writeToTextWithoutBuffer(){
+    public void writeToTextWithoutBuffer() {
         ts.init();
         ts.setBegin("Begin write text without buffer");
         String fileName = "C:\\test\\lv" + String.valueOf(level) + ".edgystringyboi";
         try (PrintWriter writer = new PrintWriter(new OutputStreamWriter(new FileOutputStream(fileName), "utf-8"))) {
-            for(Edge e : edges){
+            for (Edge e : edges) {
                 writer.print(e.X1 + "," + e.Y1 + "," + e.X2 + "," + e.Y2 + ";");
             }
             System.out.println("Wegschrijven text zonder buffer geslaagd");
         } catch (IOException e) {
             e.printStackTrace();
-        }
-        finally {
+        } finally {
             ts.setEnd("End");
             System.out.println(ts.toString());
         }
     }
 
-    public void writeToTextWithBuffer(){
+    public void writeToTextWithBuffer() {
         ts.init();
         ts.setBegin("Begin write text with buffer");
         String fileName = "C:\\test\\blv" + String.valueOf(level) + ".edgystringyboi";
         try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName), "utf-8"))) {
-            for(Edge e : edges){
+            for (Edge e : edges) {
                 writer.append(e.X1 + "," + e.Y1 + "," + e.X2 + "," + e.Y2 + ";");
             }
             System.out.println("Wegschrijven text met buffer geslaagd");
         } catch (IOException e) {
             e.printStackTrace();
-        }
-        finally {
+        } finally {
             ts.setEnd("End");
             System.out.println(ts.toString());
         }
@@ -108,6 +129,6 @@ public class Main implements Observer{
 
     @Override
     public void update(Observable o, Object arg) {
-        edges.add((Edge)arg);
+        edges.add((Edge) arg);
     }
 }
